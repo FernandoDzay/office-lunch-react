d4c92935c06a4675abdb5736ae2544d28d70e87e
FernandoDzay <luisf.dzay@gmail.com>
2022-06-03 20:41:09 -0500

    Arreglando css auth

--- src/components/login/AuthForm.js
+++ src/components/login/AuthForm.js
@@ -6,14 +6,16 @@ import Button from '../globals/Button/Button';
 export default function AuthForm({title, href, linkText, submitText, submitHandler, loading, children}) {
 
     return (
-        <form className="auth-form">
-            <i className="zmdi zmdi-account-circle zmdi-hc-5x"></i>
-            <p className="title">{title}</p>
-            
-            {children}
+        <div className="auth-form">
+            <form>
+                <i className="zmdi zmdi-account-circle zmdi-hc-5x"></i>
+                <p className="title">{title}</p>
+                
+                {children}
 
-            <Link to={href} className="link">{linkText}</Link>
-            <Button color="red" loading={loading} onClick={submitHandler}>{submitText}</Button>
-        </form>
+                <Link to={href} className="link">{linkText}</Link>
+                <Button color="red" loading={loading} onClick={submitHandler}>{submitText}</Button>
+            </form>
+        </div>
     );
 }
